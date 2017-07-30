39f7b066dd4728853260a0353d0db50577549f42
Emmet Murray <ehmurray8@gmail.com>
2017-07-29 20:30:12 -0400

    Added calls to rpi_scripts

--- auto_home_msg_handler.js
+++ auto_home_msg_handler.js
@@ -1,4 +1,9 @@
 var PubNub = require('pubnub');
+var shell = require('shelljs');
+
+var scriptsPath = "~/AutoHome/rpi_scripts/";
+var scriptName = "";
+var params = "";
 
 function subscribe() {
    
@@ -14,10 +19,36 @@ function subscribe() {
         },
         message: function(message) {
             console.log("New Message!!", message);
-            func = message.message["Function Name"];
-            if(func == "Channel") {
-                
+            var innerMsg = message.message;
+            var func = innerMsg["Function Name"];
+            if (func == "Channel") {
+                scriptName = "chng_channel";
+                if(innerMsg.hasOwnProperty("Channel Number")) {
+                    var chanNum = innerMsg["Channel Number"];        
+                    params = chanNum.toString().split('').join(' ');
+                } else {
+                    params = innerMsg["Direction"].toLowerCase() + " " + Number(innnerMsg["Number"]);
+                }
+            } else if (func == "Volume") {
+                scriptName = "chng_volmue";
+                params = innerMsg["Direction"].toLowerCase() + " " + Number(innnerMsg["Number"]);
+            } else if (func == "Input") {
+                scriptName = "chng_input";
+                params = innerMsg["Direction"].toLowerCase() + " " + Number(innnerMsg["Number"]);
+            } else if (func == "Key") {
+                scriptName = "key";
+                params = innerMsg["Key Type"];
+            } else if (func == "Socket") {
+                scriptName = "socket";
+                if (innerMsg.hasOwnProperty("Socket State")) {
+                    params = Number(innerMsg["Socket Type"]) + " " + innerMsg["Socket State"].toLowerCase(); 
+                } else {
+                    scriptName = "Key";
+                    params = "POWER";
+                }
             }
+
+            shell.exec(scriptsPath + scriptName + " " + params);
         },
         presence: function(presenceEvent) {
             // handle presence

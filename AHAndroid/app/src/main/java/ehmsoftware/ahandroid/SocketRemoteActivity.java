e2ab1ed2aa997592bc8f5d5ceb6dc4a8749fe3cc
ehmurray8 <ehmurray8@gmail.com>
2017-08-23 13:35:10 -0400

    Refactored directory structure, added Android App.

---
+++ AHAndroid/app/src/main/java/ehmsoftware/ahandroid/SocketRemoteActivity.java
@@ -0,0 +1,15 @@
+package ehmsoftware.ahandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SocketRemoteActivity extends AppCompatActivity {
+
+    final
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_socket_remote);
+    }
+}
---
+++ AHAndroid/app/src/main/java/ehmsoftware/ahandroid/TVRemoteActivity.java
@@ -0,0 +1,15 @@
+package ehmsoftware.ahandroid;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class TVRemoteActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tvremote);
+    }
+
+
+}

e4a1bc5159f312347772ffbe070667d75ef93ab2
Emmet Murray <ehmurray8@gmail.com>
2018-05-30 12:37:21 -0400

    Running addgw script frequently.

--- rpi_scripts/addgw.sh
+++ rpi_scripts/addgw.sh
@@ -3,8 +3,10 @@
 PATH=/sbin:/bin:/usr/sbin:/usr/bin:/usr/local/sbin:/usr/local/bin
 
 host=`/bin/hostname -I`
-if [[ $host =~ 10.0.0.* ]]; then
-    /sbin/route add default gw 10.0.0.1
-fi
-
+while [ 1 ]; do
+    if [[ $host =~ 10.0.0.* ]]; then
+        /sbin/route add default gw 10.0.0.1
+    fi
+    sleep 30
+done
 exit 1

8c9aaba580cb900890c7ea50860d7210125c26ec
Emmet Murray <ehmurray8@gmail.com>
2019-03-03 17:02:39 -0600

    Added debugging info.

--- rpi_scripts/auto_home_startup.sh
+++ rpi_scripts/auto_home_startup.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 node /home/pi/AutoHome/rpi_scripts/auto_home_msg_handler.js &
-node /home/pi/AutoHome/rpi_scripts/awake_handler.js &
+node /home/pi/AutoHome/rpi_scripts/awake_handler.js > ~/AutoHome/rpi_scripts/logs/awake_handler
--- rpi_scripts/awake_handler.js
+++ rpi_scripts/awake_handler.js
@@ -7,8 +7,10 @@ var scriptsPath = "/home/pi/AutoHome/rpi_scripts/";
 var realtime = new Ably.Realtime({key: ably_info.ABLY_KEY});
 var channel = realtime.channels.get("AWAKE_CHANNEL");
 
+console.log("Connecting...");
 
 channel.subscribe(function(message) {
+    console.log(message);
     var scriptName = "alarm_script";
     var cmd = scriptsPath + scriptName;
     spawn('bash', cmd.split(' '), {stdio: 'inherit'});
